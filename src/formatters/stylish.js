f08cb7f03fcf60af7d05cfc52578c4eee667a051
Демид Хабаров <piratnabortu@gmail.com>
2023-11-27 10:05:09 +0300

    almost

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -17,7 +17,7 @@ function stringify(data, depth) {
     return String(data);
   }
   const keys = _.keys(data);
-  const lines = keys.map((key) => `${forwardIndent(depth)}  ${key}: ${stringify(data[key], depth)}`);
+  const lines = keys.map((key) => `${forwardIndent(depth)}  ${key}: ${stringify(data[key], depth + 1)}`);
   console.log(lines);
   return joinStrings(lines, depth);
 }
